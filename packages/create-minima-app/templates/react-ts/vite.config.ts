5763d2f29fe1f0c8e55f0d37d7d1a1b2a45ae0fa
dynamitesushi <winson.hero@gmail.com>
2025-02-06 12:33:49 +0000

    changelog file will now be copied to the build folder

--- packages/create-minima-app/templates/react-ts/vite.config.ts
+++ packages/create-minima-app/templates/react-ts/vite.config.ts
@@ -1,6 +1,7 @@
 import { TanStackRouterVite } from "@tanstack/router-plugin/vite"
 import legacy from "@vitejs/plugin-legacy"
 import react from "@vitejs/plugin-react-swc"
+import { copyFileSync } from "fs"
 import path from "path"
 import { defineConfig } from "vite"
 
@@ -20,5 +21,17 @@ export default defineConfig({
     legacy({
       targets: ["defaults", "not IE 11", "Android >= 9"],
     }),
+    {
+      name: "copy-changelog",
+      closeBundle() {
+        try {
+          copyFileSync("CHANGELOG.md", "build/CHANGELOG.md")
+        } catch (error) {
+          console.warn(
+            "Could not copy CHANGELOG.md, please check that it exists in the root directory"
+          )
+        }
+      },
+    },
   ],
 })
